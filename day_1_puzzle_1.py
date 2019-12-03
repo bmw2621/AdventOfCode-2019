9ad679e86db3148f574ef91e83e791854a903049
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-12-03 13:08:12 -0500

    add day 1 puzzle 1

---
+++ day_1_puzzle_1.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+req = urllib.request.Request(url='https://adventofcode.com/2019/day/1/input')
+req.add_header("Cookie", #SESSION COOKIE FROM BROWSER)
+with urllib.request.urlopen(req) as f:
+    data = f.read().decode('utf-8')
+
+data = data.split("\n")
+data = data[:-1]
+
+sum_of_requirements = 0
+
+for line in data:
+    mass = int(line)
+    sum_of_requirements += (mass//3)-2
+
+print(sum_of_requirements)
