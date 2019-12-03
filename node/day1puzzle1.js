fe6c1128b0b9998992094777c3b1bf3dadeacff3
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-12-03 17:06:26 -0500

    node

--- node/day1puzzle1.js
+++ node/day1puzzle1.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 const protectedUrl = "https://adventofcode.com/2019/day/1/input"
-const cookie = 'session=53616c7465645f5ff85d2293bbb71faf9caeca66488104faf5a9521d283e71820d260467637fc8429493184cdd9a020c'
+const cookie = 'session=/*ADD SESSION COOKIE HERE*/'
 const arrSum = arr => arr.reduce((a,b) => a + b, 0)
 
 const getInput = async () => {
